75d3a88c43353e4814c9e6507903f6c5e1f92507
Jonathan Huang <jonathannhuang@gmail.com>
2024-06-05 07:31:50 -0400

    added target file for testing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,16 @@
 use std::env; // used for reading command line arguments
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    // .collect turns it into a vector
+    // env::args() basically reads the argument from the command line
+    let args: Vec<String> = env::args().collect(); 
+    dbg!(&args); // print args in debug 
+
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for {} filepath", query);
+    println!("In file {}", filepath);
+
+    
 }
