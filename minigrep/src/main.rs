1bf8d723ccd0d620a2f87981fe826f0627c8a88a
Jonathan Huang <jonathannhuang@gmail.com>
2024-06-05 07:48:18 -0400

    abstraction 1: create a function to parse config, abstraction 2: turn returned values as a struct object, abstraction 3: make it obvious you are returning a struct object by turning it into a impl

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,14 +8,40 @@ fn main() {
     let args: Vec<String> = env::args().collect(); 
     dbg!(&args); // print args in debug 
 
-    let query = &args[1];
-    let filepath = &args[2];
+    let config = parse_config(args)
 
-    println!("Searching for {} filepath", query);
-    println!("In file {}", filepath);
+    println!("Searching for {} filepath", config.query);
+    println!("In file {}", config.file_path);
 
     let contents = fs::read_to_string(filepath)
         .expect("Should be able to read the file");
     
     println!("\nWith text: \n{contents}");
+}
+
+// create a way to make it so creading Config struct is more obvious
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+
+        Config {query, filepath}
+    }
+}
+
+/*
+// this returns the struct defined below
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // want to take ownership because Config struct is supposed to own its strings
+    let query = args[1].clone();
+    let filepath = args[2].clone();
+
+    Config {query, filepath}
+}
+*/
+
+struct Config {
+    query: String,
+    file_path: String,
 }
